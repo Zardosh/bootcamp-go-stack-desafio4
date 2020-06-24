214c5a4c3d79d58d033ee940311b666bd88e0e91
Zardosh <eduzarrdo@gmail.com>
2020-06-24 00:53:05 -0300

    Create and List Transactions; Get Balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,29 +1,60 @@
 import Transaction from '../models/Transaction';
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+    income: number;
+    outcome: number;
+    total: number;
+}
+
+interface CreateTransactionDTO {
+    title: string;
+    value: number;
+    type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
-  private transactions: Transaction[];
+    private transactions: Transaction[];
 
-  constructor() {
-    this.transactions = [];
-  }
+    constructor() {
+        this.transactions = [];
+    }
 
-  public all(): Transaction[] {
-    // TODO
-  }
+    public all(): Transaction[] {
+        return this.transactions;
+    }
 
-  public getBalance(): Balance {
-    // TODO
-  }
+    public getBalance(): Balance {
+        let income = 0;
+        let outcome = 0;
 
-  public create(): Transaction {
-    // TODO
-  }
+        this.transactions.forEach(transaction => {
+            if (transaction.type === 'income') {
+                income += transaction.value;
+            } else {
+                outcome += transaction.value;
+            }
+        });
+
+        const balance = {
+            income,
+            outcome,
+            total: income - outcome,
+        };
+
+        return balance;
+    }
+
+    public create({ title, value, type }: CreateTransactionDTO): Transaction {
+        const transaction = new Transaction({
+            title,
+            value,
+            type,
+        });
+
+        this.transactions.push(transaction);
+
+        return transaction;
+    }
 }
 
 export default TransactionsRepository;
